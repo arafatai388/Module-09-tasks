25596a5f3eb56278a74e0c8d769957ac29b8456f
arafatai388 <arafat.cseai@gmail.com>
2026-07-17 23:03:36 +0600

    Done task 07

---
+++ task_07.js
@@ -0,0 +1,13 @@
+/*
+Task 7 — Multiplication Table 
+Ask the user for a number (or set a variable), then print its multiplication table
+ from 1 to 10 using a for loop. Example: if the number is 7 → 7 x 1 = 7, 7 x 2 = 14, ...
+  7 x 10 = 70.
+
+*/
+
+let number=7;
+for(let i=1;i<=10;i++)
+{
+    console.log(number,"X",i,"=",number*i);
+}
