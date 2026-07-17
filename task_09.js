55bc929dd4dd9770de66b3f59d2d0006c04cbe6e
arafatai388 <arafat.cseai@gmail.com>
2026-07-17 23:09:12 +0600

    Done task 09

---
+++ task_09.js
@@ -0,0 +1,12 @@
+/*
+Task 9 — Sum of Even Numbers
+Write a for loop that calculates the sum of all even numbers from 2 to 50.
+
+*/
+
+let sum =0;
+for(let i=2;i<=50;i+=2)
+{
+    sum=sum+i;
+}
+console.log("Sum=",sum);
